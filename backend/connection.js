f6877fa149b18abff2dc7dc973c75a774c5d6a03
Pallavi Verulkar <verulkarpallavi1686@gmail.com>
2026-01-02 13:15:30 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 async function connectMongoDB() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected");
-    } catch (err) {
-        console.log("Error connecting to MongoDB:", err.message);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log("Error connecting to MongoDB:", err.message);
+  }
 }
 
 module.exports = { connectMongoDB };
