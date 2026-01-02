f83e9321630fd808de84a45fc1669e0bee44eefa
Pallavi Verulkar <verulkarpallavi1686@gmail.com>
2026-01-02 13:14:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const { connectMongoDB } = require("./connection");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const userRoute = require("./routes/user");
+require("dotenv").config();
 
 const app = express();
 
@@ -10,17 +11,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: true,
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true
 }));
 
 app.use(cookieParser());
-
 app.use("/user", userRoute);
 
+// connect to MongoDB Atlas
 connectMongoDB();
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`server is running on ${PORT}`);
 });
